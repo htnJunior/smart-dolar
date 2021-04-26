777657a74e4d788edc60e7d62a653a6a79b702b7
htnJunior <junioor_souzaa282@hotmail.com>
2021-04-26 10:23:12 -0300

    Fixing Deployment issue

--- server/src/controllers/DataScraper.js
+++ server/src/controllers/DataScraper.js
@@ -1,9 +1,11 @@
-//const axios = require('axios')
 const puppeteer = require('puppeteer')
 
 module.exports = {
   americanDolarConsult: async (req, res) => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]})
     const page = await browser.newPage()
     await page.goto('https://www.melhorcambio.com/dolar-hoje')
     const dataInfo = await page.evaluate(() => {
